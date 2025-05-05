a270bd1eb9b95fcd671f849e092a91f51426a30e
joaocmezari <joaocmezari@gmail.com>
2025-05-05 19:33:17 -0300

    errando de proposito

--- aula8.py
+++ aula8.py
@@ -6,7 +6,8 @@ maior_de_idade = idade >= 18
 altura_metros = 1.80
 print("Nome: ", nome)
 print("Sobrenome: ", sobrenome)
-print("Idade: ", idade)
+print("Idade: ", idade) print oi 
+
 print("Ano de nascimento: ", ano_nascimento)
 print("É maior de idade? ", maior_de_idade)
 print("Altura em metros: ", altura_metros)
